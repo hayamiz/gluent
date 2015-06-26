36be8bd7150abb6b1a034aac26f39c043c523b4c
Yuto Hayamizu <y.hayamizu@gmail.com>
2015-06-27 02:24:22 +0900

    add match_file

--- app/helpers/search.rb
+++ app/helpers/search.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 module Gluent
 
@@ -5,7 +6,17 @@ module SearchHelper
   # parse a query string into an expression tree
   # FIXME: currently returns an array of RegExp objects, which represents AND-ed conditions
   def parse_query(query_string)
-    [Regexp.compile(query_string)]
+    query_string.split(/[\s　]+/).map do |keyword|
+      Regexp.compile(keyword, Regexp::IGNORECASE)
+    end
+  end
+
+  def match_file(file_path, search_exp)
+    content = File.read(file_path)
+
+    search_exp.all? do |regexp|
+      regexp =~ content
+    end
   end
 end # SearchHelper
 
--- test/test_search_helper.rb
+++ test/test_search_helper.rb
@@ -1,8 +1,12 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 require 'rubygems'
 require 'test-unit'
 require File.expand_path("../../app/helpers/search.rb", __FILE__)
+
+def test_data_path(name)
+  File.expand_path(name, File.expand_path("../test_data", __FILE__))
+end
 
 class TestSearchHelper < Test::Unit::TestCase
   setup do
@@ -13,11 +17,73 @@ class TestSearchHelper < Test::Unit::TestCase
 
   sub_test_case 'query parsing' do
     test 'plain text' do
-      assert_equal [/abla/], @obj.parse_query("abla")
+      assert_equal [/abla/i], @obj.parse_query("abla")
     end
 
     test 'single regexp' do
-      assert_equal [/oh*/], @obj.parse_query("oh*")
+      assert_equal [/oh*/i], @obj.parse_query("oh*")
+    end
+
+    test 'two keywords' do
+      assert_equal [/hello/i, /world/i], @obj.parse_query("hello world")
+
+      # spaces do not matter
+      assert_equal [/hello/i, /world/i], @obj.parse_query("hello		 world")
+
+      # also Japanese spaces do not matter
+      assert_equal [/hello/i, /world/i], @obj.parse_query("hello　world")
+    end
+
+    test 'two regexps' do
+      assert_equal [/(a|b)/i, /hoge+/i], @obj.parse_query("(a|b) hoge+")
+
+      # spaces do not matter
+      assert_equal [/(a|b)/i, /hoge+/i], @obj.parse_query("(a|b)   	 hoge+")
+    end
+  end
+
+  sub_test_case 'file matching' do
+    test 'single English keyword' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("humpty"))
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("dumpty"))
+
+      assert_false @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                   @obj.parse_query("alice"))
+    end
+
+    test 'single English regexp' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query(".*mpty"))
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("(h|d)umpty"))
+    end
+
+    test 'two English keywords' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("humpty dumpty"))
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("humpty wall"))
+    end
+
+    test 'single Japanese keyword' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("ハンプティ"))
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("ダンプティ"))
+    end
+
+    test 'single Japanese regexp' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("(ハ|ダ)ンプティ"))
+    end
+
+    test 'two Japanese keywords' do
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("ハンプティ ダンプティ"))
+      assert_true @obj.match_file(test_data_path("humpty-dumpty.txt"),
+                                  @obj.parse_query("ハンプティ 塀"))
     end
   end
 end
